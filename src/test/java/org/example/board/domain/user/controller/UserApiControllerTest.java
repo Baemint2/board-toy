c62eee267d67e81eb3e9cfea02a2449fc9da0570
Baemint2 <qofndlwl@gmail.com>
2024-03-03 00:31:24 +0900

    비밀번호 변경 테스트

--- src/test/java/org/example/board/domain/user/controller/UserApiControllerTest.java
+++ src/test/java/org/example/board/domain/user/controller/UserApiControllerTest.java
@@ -3,17 +3,22 @@ package org.example.board.domain.user.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.board.domain.user.dto.UserCreateDto;
 import org.example.board.domain.user.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -29,8 +34,16 @@ class UserApiControllerTest {
     @MockBean
     private UserService userService;
 
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+
     @Autowired
     private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        Mockito.when(userService.checkIfValidOldPassword(anyString())).thenReturn(true);
+    }
 
     @Test
     @WithMockUser
@@ -50,5 +63,26 @@ class UserApiControllerTest {
                         .content(objectMapper.writeValueAsString(userCreateDto))
                         .with(csrf()))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser
+    public void 비밀번호_변경_확인() throws Exception {
+        //Given
+        String requestBody = """
+                {
+                    "currentPassword": "oldPassword",
+                    "newPassword": "newPassword123",
+                    "confirmPassword": "newPassword123"
+                }
+                """;
+        //When && Then
+        mockMvc.perform(post("/api/v1/user/password/reset")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody)
+                .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("비밀번호가 성공적으로 변경되었습니다."));
+
     }
 }
