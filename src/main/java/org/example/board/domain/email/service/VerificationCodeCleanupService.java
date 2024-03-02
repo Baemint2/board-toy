aa01f81c26d60bac1183fd3cb918bcc41234c8af
Baemint2 <qofndlwl@gmail.com>
2024-03-02 23:27:22 +0900

    임시 사용자 테이블의 인증코드도 자정마다 삭제

--- src/main/java/org/example/board/domain/email/service/VerificationCodeCleanupService.java
+++ src/main/java/org/example/board/domain/email/service/VerificationCodeCleanupService.java
@@ -3,6 +3,7 @@ package org.example.board.domain.email.service;
 import lombok.RequiredArgsConstructor;
 import org.example.board.domain.email.entity.VerificationCode;
 import org.example.board.domain.email.repository.VerificationRepository;
+import org.example.board.domain.user.repository.TemporaryRepository;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +13,21 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class VerificationCodeCleanupService {
-    private VerificationRepository verificationRepository;
+    private final VerificationRepository verificationRepository;
+    private final TemporaryRepository temporaryRepository;
 
     @Scheduled(cron = "0 0 0 * * *")
     public void cleanupExpiredVerificationCodes() {
         LocalDateTime now = LocalDateTime.now();
         List<VerificationCode> expiredCodes = verificationRepository.findAllByExpiryDateTimeBefore(now);
         verificationRepository.deleteAll(expiredCodes);
+        cleanupExpiredTemporaryUsers(now);
+
     }
+
+    public void cleanupExpiredTemporaryUsers(LocalDateTime now) {
+        temporaryRepository.findAllByExpiryDateTimeBefore(now);
+
+    }
+
 }
--- src/main/java/org/example/board/domain/user/repository/TemporaryRepository.java
+++ src/main/java/org/example/board/domain/user/repository/TemporaryRepository.java
@@ -3,9 +3,11 @@ package org.example.board.domain.user.repository;
 import org.example.board.domain.user.entity.TemporaryUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TemporaryRepository extends JpaRepository<TemporaryUser, Long> {
-
     Optional<TemporaryUser> findByEmail(String email);
+
+    void findAllByExpiryDateTimeBefore(LocalDateTime now);
 }
