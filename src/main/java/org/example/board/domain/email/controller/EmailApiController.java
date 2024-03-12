b9cf78f7bfc39b626e10af7ecca29706c09045b6
Baemint2 <qofndlwl@gmail.com>
2024-03-13 07:33:00 +0900

    파라미터 Map에서 DTO로 변경

--- src/main/java/org/example/board/domain/email/controller/EmailApiController.java
+++ src/main/java/org/example/board/domain/email/controller/EmailApiController.java
@@ -1,7 +1,10 @@
 package org.example.board.domain.email.controller;
 
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.board.domain.email.dto.EmailVerificationDto;
 import org.example.board.domain.email.service.AuthenticationService;
 import org.example.board.domain.email.service.VerificationService;
 import org.example.board.domain.user.entity.EmailVerification;
@@ -25,8 +28,8 @@ public class EmailApiController {
     private final EmailVerificationRepository emailVerificationRepository;
     // 인증 코드 전송
     @PostMapping("/api/email/sendVerificationCode")
-    public ResponseEntity<?> sendVerificationCode(@RequestBody Map<String, String> request) {
-        String email = request.get("email");
+    public ResponseEntity<?> sendVerificationCode(@Valid @RequestBody EmailVerificationDto verificationDto) {
+        String email = verificationDto.getEmail();
 
         try {
             authenticationService.sendAndSaveVerificationCode(email);
@@ -39,9 +42,9 @@ public class EmailApiController {
 
     // 인증 코드 검증
     @PostMapping("/api/email/verifyCode")
-    public ResponseEntity<?> verifyCode(@RequestBody Map<String, String> request) {
-        String email = request.get("email");
-        String code = request.get("code");
+    public ResponseEntity<?> verifyCode(@Valid @RequestBody EmailVerificationDto verificationDto, HttpSession session) {
+        String email = verificationDto.getEmail();
+        String code = verificationDto.getCode();
         log.info("인증 코드 검증 요청: email={}, code={}", email, code);
         try {
             Optional<EmailVerification> optionalTemporaryUser = emailVerificationRepository.findByEmail(email);
@@ -56,14 +59,15 @@ public class EmailApiController {
             boolean isCodeValid = verificationService.verifyCode(email, code);
             if (isCodeValid) {
                 log.info("인증 성공: email={}", email);
+                session.setAttribute("verifiedEmail", email);
                 return ResponseEntity.ok().body(Map.of("message", "인증 성공"));
             } else {
                 log.info("인증 실패: email={}", email);
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "인증 실패"));
+                return ResponseEntity.badRequest().body(Map.of("error", "인증 실패"));
             }
         } catch (Exception ex) {
             log.error("인증 코드 검증 중 오류 발생: email={}, error={}", email, ex.getMessage(), ex);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "인증 코드 검증 중 오류가 발생했습니다."));
+            return ResponseEntity.internalServerError().body(Map.of("error", "인증 코드 검증 중 오류가 발생했습니다."));
         }
     }
 }
